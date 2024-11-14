b12b6ee36825ce8ebbc08238c679afcf1eed49f8
GunjanK01 <gunjankhadatkar23@gmail.com>
2024-11-14 19:25:16 +0530

    Initial commit of Shopee Flutter e-commerce app

---
+++ lib/core/themes/app_color.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color appPrimaryColor = const Color(0xFF253745);
+  static Color appSecondaryColor = const Color(0xFFCCD0CF);
+  static const scaffoldBackGroundColorLight = Colors.white;
+  static const Color scaffoldBackgroundColorDark = Color(0xFF11212D);
+  static const Color darkComponentsColor = Color(0xFF06141B);
+  static Color appBarActionButtonColorLight = const Color(0xFFF3F3F3);
+  static Color appBarActionButtonColorDark = const Color(0xFF282828);
+  static Color searchBarBackgroundColor = const Color(0xFFF3F3F3);
+  static Color ratingIconColor = const Color(0xFFF9D858);
+  static Color productCarouselBackgroundColor = const Color(0xFF9BA8AB);
+  static Color quantityDecreaseCardColor = const Color(0xFF253745);
+  static Color productCardImageBackgroundColor = Color(0xFFCCD0CF);
+}
